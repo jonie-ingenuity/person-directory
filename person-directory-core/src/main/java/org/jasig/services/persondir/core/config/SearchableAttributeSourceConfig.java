95e89be2420c40e9d8ba94a1f84480fa3feb589a
Eric Dalquist <eric.dalquist@gmail.com>
2012-12-26 14:40:50 -0600

    Fix access modifier

--- person-directory-core/src/main/java/org/jasig/services/persondir/core/config/SearchableAttributeSourceConfig.java
+++ person-directory-core/src/main/java/org/jasig/services/persondir/core/config/SearchableAttributeSourceConfig.java
@@ -2,7 +2,7 @@ package org.jasig.services.persondir.core.config;
 
 import org.jasig.services.persondir.spi.BaseAttributeSource;
 
-interface SearchableAttributeSourceConfig<S extends BaseAttributeSource>
+public interface SearchableAttributeSourceConfig<S extends BaseAttributeSource>
         extends AttributeSourceConfig<S>{
 
     int getMaxResults();
